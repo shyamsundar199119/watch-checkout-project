c4de9bc9b73e79cf97258626008a170a0d3fdece
Shyam Sundar <shyamsundar199119@gmail.com>
2026-01-14 19:20:08 +0100

    Added initial positive test case

--- src/main/java/eu/handelsbanken/domain/CheckoutResponse.java
+++ src/main/java/eu/handelsbanken/domain/CheckoutResponse.java
@@ -3,13 +3,20 @@ package eu.handelsbanken.domain;
 import java.util.List;
 
 public class CheckoutResponse {
-    List<WatchDto> watchDtoList;
+    private List<WatchDto> watchDtoList;
+    private int totalPrice;
 
-    public CheckoutResponse(List<WatchDto> watchDtoList) {
+
+    public CheckoutResponse(List<WatchDto> watchDtoList, int totalPrice) {
         this.watchDtoList = watchDtoList;
+        this.totalPrice=totalPrice;
     }
 
     public List<WatchDto> getWatchDtoList() {
         return watchDtoList;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
     }
 }
---
+++ src/test/java/eu/handelsbanken/service/CheckoutServiceTest.java
@@ -0,0 +1,45 @@
+package eu.handelsbanken.service;
+
+import eu.handelsbanken.domain.CheckoutResponse;
+import eu.handelsbanken.domain.WatchDto;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckoutServiceTest {
+
+    private CheckoutService checkoutService = new CheckoutService();
+
+
+    @Test
+    public void shouldApplyDiscountPriceForOneProduct() {
+        CheckoutResponse checkoutResponse = checkoutService.calculateCheckoutPrice(List.of(new WatchDto("001", 3)));
+        assertEquals(200, checkoutResponse.getTotalPrice());
+    }
+
+    @Test
+    public void shouldCalculateUnitPriceWhenNoDiscountRuleExists(){
+        CheckoutResponse checkoutResponse = checkoutService.calculateCheckoutPrice(List.of(new WatchDto("001", 1)));
+        assertEquals(100, checkoutResponse.getTotalPrice());
+    }
+
+    @Test
+    public void shouldAddDiscountedAndUnitPrice(){
+        CheckoutResponse checkoutResponse = checkoutService.calculateCheckoutPrice(List.of(new WatchDto("001", 3), new WatchDto("003", 1)));
+        assertEquals(250, checkoutResponse.getTotalPrice());
+    }
+
+    @Test
+    public void shouldApplyDiscountForTwoProduct(){
+        CheckoutResponse checkoutResponse = checkoutService.calculateCheckoutPrice(List.of(new WatchDto("001", 3), new WatchDto("002", 2)));
+        assertEquals(320, checkoutResponse.getTotalPrice());
+    }
+
+    @Test
+    public void shouldApplyDiscountMutlipleTimes(){
+        CheckoutResponse checkoutResponse = checkoutService.calculateCheckoutPrice(List.of(new WatchDto("001", 6)));
+        assertEquals(400, checkoutResponse.getTotalPrice());
+    }
+}
